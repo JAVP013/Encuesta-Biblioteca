7d920cb406a0b365c24b93272efc8f4c54d67727
KennRobot <kebrgonzalezdo@ittepic.edu.mx>
2024-09-18 15:41:26 -0700

    Funciona todo ya con lo que pidio Norma

--- Pagina/script.js
+++ Pagina/script.js
@@ -27,34 +27,43 @@ formulario.addEventListener('submit', function(event) {
 /* PARA LAS PREGUNTAS 13,14 Y 15 */
 document.addEventListener('DOMContentLoaded', function() {
     // Seleccionar las opciones de las preguntas
-    let pregunta13_no = document.getElementById('p13_no');
-    let pregunta13_no_enterado = document.getElementById('p13_no_enterado');
+    let preg13_si = document.getElementById('preg13_si');
+    let preg13_no = document.getElementById('preg13_no');
+    let preg13_no_enterado = document.getElementById('preg13_noenterado');
     
-    let pregunta14_si = document.getElementById('p14_si');
-    let pregunta15_si = document.getElementById('p15_si');
-    
-    let pregunta14_no = document.getElementById('p14_no');
-    let pregunta15_no = document.getElementById('p15_no');
+    let preg14_siempre = document.getElementById('preg14_siempre');
+    let preg14_23veces = document.getElementById('preg14_23veces');
+    let preg14_1vez = document.getElementById('preg14_1vez');
+    let preg14_nunca = document.getElementById('preg14_nunca');
+
+    let preg15_no = document.getElementById('preg15_no');
+    let preg15_si = document.getElementById('preg15_si');
   
     // Escuchar el cambio en la Pregunta 13
-    document.getElementsByName('pregunta13').forEach((element) => {
+    document.getElementsByName('preg13').forEach((element) => {
       element.addEventListener('change', function() {
-        if (pregunta13_no.checked || pregunta13_no_enterado.checked) {
+        if (preg13_no.checked || preg13_no_enterado.checked) {
           // Si selecciona "No" o "No enterado" en Pregunta 13, marcar las respuestas en Pregunta 14 y 15
-          pregunta14_no.checked = true;
-          pregunta15_no.checked = true;
+          preg14_nunca.checked = true;
+          preg15_no.checked = true;
   
           // Deshabilitar las opciones de Pregunta 14 y 15
-          pregunta14_si.disabled = true;
-          pregunta14_no.disabled = true;
-          pregunta15_si.disabled = true;
-          pregunta15_no.disabled = true;
+          preg14_siempre.disabled = true;
+          preg14_23veces.disabled = true;
+          preg14_1vez.disabled = true;
+          preg15_si.disabled = true;
+          preg15_no.disabled = true;
         } else {
           // Habilitar las opciones de Pregunta 14 y 15 si no se selecciona "No" o "No enterado"
-          pregunta14_si.disabled = false;
-          pregunta14_no.disabled = false;
-          pregunta15_si.disabled = false;
-          pregunta15_no.disabled = false;
+          preg14_siempre.disabled = false;
+          preg14_23veces.disabled = false;
+          preg14_1vez.disabled = false;
+          preg15_si.disabled = false;
+          preg15_no.disabled = false;
+        }
+        if(preg13_si.checked){
+            // Si selecciona "si" en Pregunta 13 deshabilita la opcion de nunca de la pregunta 14
+            preg14_nunca.disabled = true;
         }
       });
     });
